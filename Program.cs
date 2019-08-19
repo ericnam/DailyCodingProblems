425cf3de7829e4beb9c55043c3438a1935d3c722
ericnam <ericnam860@gmail.com>
2019-08-18 21:00:47 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DailyCodingProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHeader();
+
+            Console.WriteLine("Input problem #:");
+
+            bool exitApplication = false;
+
+            while (!exitApplication) {
+
+            }
+
+            string codingProblemNumber = Console.ReadLine();
+        }
+
+        static void CreateHeader () {
+            const string headerText = "Daily Coding Challenges";
+            Console.WriteLine();
+            Console.WriteLine(headerText);
+            Console.WriteLine(new String('-', headerText.Length));
+            Console.WriteLine();
+            Console.WriteLine("Input the ");
+            
+        }
+    }
+
+}
