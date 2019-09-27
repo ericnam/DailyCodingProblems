a6c14c4b9c779100585aba6b82beab94b2185014
ericnam <ericnam860@gmail.com>
2019-09-26 22:44:42 -0500

    LIS

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace DailyCodingProblems
             CreateHeader();
             while (true) {
                 Console.WriteLine("Input problem #:");
-                string userInput = "search";
+                string userInput = "lis";
                 int problemNumber = 0;
 
                 if (userInput == "sort") {
@@ -24,6 +24,10 @@ namespace DailyCodingProblems
                 else if (userInput == "Fib") {
                     Fib fib = new Fib();
                     fib.Main();
+                }
+                else if (userInput == "lis") {
+                    LISS lis = new LISS();
+                    lis.Main();
                 }
                 else if (userInput == "search") {
                     SearchingAlgorithms search = new SearchingAlgorithms();
